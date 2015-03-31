4354e95dda7f9f4cb23d15ba85f2de919b047914
watttman <manul91@abv.bg>
2015-03-31 10:43:53 -0400

    Install composer and pristine Yii2 basic template, fix reset-website-dirs.sh script not to conflict when run on existing Yii2 site template installed

--- configuration-history/050-install-apache2-webserver-and-php5/reset-website-dirs.sh
+++ configuration-history/050-install-apache2-webserver-and-php5/reset-website-dirs.sh
@@ -36,7 +36,7 @@ echo "--> Touching ${BASEWEBDIR} to ensure it exists..."
 mkdir -p ${BASEWEBDIR} || { echo "Can't mkdir -p base user web dir ${BASEWEBDIR}"; exit 1; }
 echo
 echo "--> Touching ${ABS_SITE_DIR}/{web,tmp,session} to ensure it exists..."
-mkdir -p ${ABS_SITE_DIR}/{web,tmp,session,log} || { echo "Can't mkdir -p the vhost site dir ${ABS_SITE_DIR}/{web,tmp,session} hierarchy"; exit 1; }
+mkdir -p ${ABS_SITE_DIR}/{web,tmp,session,log} || { echo "Can't mkdir -p the vhost site dir ${ABS_SITE_DIR}/{web,tmp,session,log} hierarchy"; exit 1; }
 
 
 
@@ -45,7 +45,7 @@ mkdir -p ${ABS_SITE_DIR}/{web,tmp,session,log} || { echo "Can't mkdir -p the vho
 echo
 echo "--> Setting test index.php file in root ..."
 # create base index.asp, unless it already exists
-[ -f ${ABS_SITE_DIR}/web/index.php ] || cat > ${ABS_SITE_DIR}/web/index.php <<DELIMITER
+[ -f ${ABS_SITE_DIR}/web/index.php ] && echo "/web/index.php already exists, skipping creation of sample index.php file... " || cat > ${ABS_SITE_DIR}/web/index.php <<DELIMITER
 <?php
         # start session to test session writing
         session_start();
@@ -82,7 +82,10 @@ echo
 echo "--> Fixing filesystem permissions for vhost site dir..."
 
 # set siteuser:www-data + sticky guid bit to dirs so all created belongs to apache group
-chown -R ${SITEUSER}:www-data ${ABS_SITE_DIR}
+# fix: do not touch any dirs out of {tmp,log,session} !
+# chown -R ${SITEUSER}:www-data ${ABS_SITE_DIR}
+chown ${SITEUSER}:www-data ${ABS_SITE_DIR}
+chown ${SITEUSER}:www-data ${ABS_SITE_DIR}/{tmp,log,session}
 
 # set readwrite:readwrite siteuser:www-data so both apache/php can write in tmp,log
 chmod -R u=rwX,g=rwX,o-rwx ${ABS_SITE_DIR}/{tmp,log}
@@ -96,8 +99,9 @@ chmod g+s ${ABS_SITE_DIR}/session
 # chmod -R u=rw,g-rwx,o-rwx ${ABS_SITE_DIR}/session/*
 
 # set rewrite:readonly siteuser:www-data so apache/php can readonly in web
-chmod -R u=rwX,g=rX,o-rwx ${ABS_SITE_DIR}/web
-find ${ABS_SITE_DIR}/web -type d -exec chmod g+s {} +
+# TODO hmmm, we may try to re-enable this? to verify after Yii2 template install
+# chmod -R u=rwX,g=rX,o-rwx ${ABS_SITE_DIR}/web
+# find ${ABS_SITE_DIR}/web -type d -exec chmod g+s {} +
 
 
 
